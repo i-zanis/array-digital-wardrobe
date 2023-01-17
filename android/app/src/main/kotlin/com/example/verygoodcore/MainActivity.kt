4ca5f49a8fe7caf50f207804bfec84cb06f7666d
i-zanis <ioanlafazanis@gmail.com>
2023-01-17 23:01:49 +0000

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.wardrobe_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:wardrobe_frontend/app/app.dart';
+import 'package:wardrobe_frontend/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
