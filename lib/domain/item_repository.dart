fb30f9ab3ddd86cf132607a617ef790973b95834
i-zanis <ioanlafazanis@gmail.com>
2023-01-30 23:07:06 +0000

    refactor: change param ItemRepository findAll() to userId

--- lib/domain/item_repository.dart
+++ lib/domain/item_repository.dart
@@ -1,15 +1,13 @@
 import 'package:wardrobe_frontend/domain/entity/item.dart';
 
 abstract class ItemRepository {
-  Future<List<Item>> findAll(int id);
+  Future<List<Item>> findAll(int userId);
 
   Future<Item> findById(int id);
 
   Future<void> delete(int id);
 
   Future<void> save(Item item);
-
-  Future<void> saveAll(List<Item> items);
 
   Future<void> update(Item item);
 }
--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,19 @@
-import 'package:wardrobe_frontend/app/app.dart';
 import 'package:wardrobe_frontend/bootstrap.dart';
+import 'package:wardrobe_frontend/rest/app/view/app.dart';
+import 'package:wardrobe_frontend/rest/util/log.dart';
 
-void main() {
-  bootstrap(() => const App());
+
+final logger = Log.instance;
+Future<void> main() async {
+  await bootstrap(() => const App());
 }
+
+// void main() {
+//   runApp(BlocProvider<WardrobeBloc>(
+//     create: (context) => WardrobeBloc(
+//       itemBloc: ItemBloc(),
+//       userBloc: UserBloc(),
+//     ),
+//     child: MyApp(),
+//   ));
+// }
