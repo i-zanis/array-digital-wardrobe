ea3f0c8a16ec3551f12c46c3fc753844ffc19ee0
i-zanis <ioanlafazanis@gmail.com>
2023-02-04 13:39:37 +0000

    doc: add comments to Logger class

--- lib/rest/util/log.dart
+++ lib/rest/util/log.dart
@@ -1,5 +1,7 @@
 import 'package:logger/logger.dart';
 
+// Log class to provide a singleton instance of the logger
+// In production mode, the logger will be disabled
 
 class Log {
   static final Logger _instance = Logger(
