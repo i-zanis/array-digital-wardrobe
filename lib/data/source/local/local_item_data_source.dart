dfeb67bd657a08a3715e6114a22257b68a33efc6
i-zanis <ioanlafazanis@gmail.com>
2023-02-07 18:26:26 +0000

    fix: encode JSON string properly when data saved to shared pref (FormatException)

--- lib/data/source/local/local_item_data_source.dart
+++ lib/data/source/local/local_item_data_source.dart
@@ -1,37 +1,68 @@
+import 'dart:convert';
+
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wardrobe_frontend/domain/entity/item.dart';
+import 'package:wardrobe_frontend/main_development.dart';
 
 class LocalItemDataSource {
-  LocalItemDataSource(this._prefs);
+  factory LocalItemDataSource() {
+    return _singleton;
+  }
 
+  // _internal is a private constructor You can create any private constructor
+  // using any Class._someName construction
+  LocalItemDataSource._internal();
+
+  static final LocalItemDataSource _singleton = LocalItemDataSource._internal();
+  static final SharedPreferences? _prefs = sharedPrefs;
   static const String _itemsKey = 'items';
-  final SharedPreferences _prefs;
 
-  Future<void> save(List<Item> items) async {
+  Future<void> saveAll(List<Item> items) async {
     final itemsJson = items.map((item) => item.toJson()).toList();
-    await _prefs.setStringList(_itemsKey, itemsJson.cast<String>());
+    logger.i('LocalItemDataSource: Saving all items: $itemsJson');
+    await _prefs?.setStringList(
+        _itemsKey, itemsJson.map((e) => json.encode(e)).toList());
+  }
+
+  Future<void> save(Item item) async {
+    final items = await findAll();
+    logger.i('LocalItemDataSource: Saving item: ${item.toJson()}');
+    final itemsWithoutUpdated = items.where((i) => item.id != i.id).toList()
+      ..add(item);
+    await saveAll(itemsWithoutUpdated);
   }
 
   Future<List<Item>> findAll() async {
-    final itemsJson = _prefs.getStringList(_itemsKey);
+    logger.d('$LocalItemDataSource: Retrieving all items');
+    final itemsJson = _prefs?.getStringList(_itemsKey);
     if (itemsJson == null) {
+      logger.i('No items found in local storage');
       return [];
     }
     return itemsJson
-        .map((item) => Item.fromJson(item as Map<String, dynamic>))
+        .map((item) => Item.fromJson(json.decode(item) as Map<String, dynamic>))
         .toList();
   }
 
   Future<void> delete(int id) async {
+    logger.i('LocalItemDataSource: Deleting item with id: $id');
     final items = await findAll();
     final itemsWithoutDeleted = items.where((i) => id != i.id).toList();
-    await save(itemsWithoutDeleted);
+    await saveAll(itemsWithoutDeleted);
   }
 
   Future<void> update(Item item) async {
+    logger.i('LocalItemDataSource: Updating item: ${item.toJson()}');
     final items = await findAll();
     final itemsWithoutUpdated = items.where((i) => item.id != i.id).toList()
       ..add(item);
-    await save(itemsWithoutUpdated);
+    await saveAll(itemsWithoutUpdated);
+  }
+
+  Future<Item> findById(int id) async {
+    logger.d('LocalItemDataSource: Retrieving item with id: $id');
+    final items = await findAll();
+    final item = items.firstWhere((i) => id == i.id);
+    return item;
   }
 }
