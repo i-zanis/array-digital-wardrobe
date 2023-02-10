eed50bae60de9516a8b6e4a117c5611c90351561
i-zanis <ioanlafazanis@gmail.com>
2023-02-10 22:04:06 +0000

    feat: add custom BottomNavBar

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,4 +1,6 @@
 class Config {
-  static const String baseUrl = 'https://localhost:8080/v1';
+  static const String baseUrl = 'http://192.168.1.104:8080/v1';
   static const String itemsUrl = '$baseUrl/items';
+  static const String weatherUrl =
+      'https://api.openweathermap.org/data/2.5/weather';
 }
