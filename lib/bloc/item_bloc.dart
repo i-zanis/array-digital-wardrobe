b9d96d64873fb47d3c0f9c5e0bf79e645479ad31
i-zanis <ioanlafazanis@gmail.com>
2023-02-14 20:01:46 +0000

    refactor: change internal methods and stitch the use cases correctly

--- lib/bloc/item_bloc.dart
+++ lib/bloc/item_bloc.dart
@@ -7,17 +7,16 @@ import '../domain/delete_item_use_case.dart';
 import '../domain/entity/item.dart';
 import '../domain/initial_load_use_case.dart';
 import '../domain/update_item_use_case.dart';
-import '../main_development.dart';
 import 'item_state.dart';
 
 class ItemBloc extends Bloc<ItemEvent, ItemState> {
-  // final LikeItemUseCase likeItemUseCase;
   ItemBloc(
-      this._saveItemUseCase, this._initialLoadUseCase, this.deleteItemUseCase)
-      : super(const ItemState.initial()) {
-    // logger.i('ItemBloc constructor');
-
-    on<ItemLoadStarted>(
+    this._initialLoadUseCase,
+    this._saveItemUseCase,
+    this._deleteItemUseCase,
+    this._updateItemUseCase,
+  ) : super(const ItemState.initial()) {
+    on<LoadItem>(
       _onLoadStarted,
       transformer: (events, mapper) => events.switchMap(mapper),
     );
@@ -35,94 +34,58 @@ class ItemBloc extends Bloc<ItemEvent, ItemState> {
     );
   }
 
-  // ItemBloc(this.addItemUseCase, this.removeItemUseCase, this.likeItemUseCase,
-  //     this.saveItemUseCase)
-  //     : super(InitialItemState());
   final InitialLoadUseCase _initialLoadUseCase;
   final SaveItemUseCase _saveItemUseCase;
-  final DeleteItemUseCase deleteItemUseCase;
+  final DeleteItemUseCase _deleteItemUseCase;
   final UpdateItemUseCase _updateItemUseCase;
 
-  Future<void> _onLoadStarted(
-      ItemLoadStarted event, Emitter<ItemState> emit) async {
-    logger.i('On Load started..');
+  Future<void> _onLoadStarted(ItemEvent event, Emitter<ItemState> emit) async {
     try {
-      emit(state.asLoading());
-      // TODO(jtl): fix with USERID
+      emit(ItemLoading(items: state.items));
       final items = await _initialLoadUseCase.execute(1);
-      emit(state.asLoadSuccess(items));
+      emit(ItemLoaded(items: items));
     } on Exception catch (e) {
-      emit(state.asLoadFailure(e));
+      emit(ItemLoadFailure(e));
     }
   }
 
   Future<void> _onSaveItem(SaveItem event, Emitter<ItemState> emit) async {
-    logger.i('On SaveItem started..');
-    emit(state.asLoading());
+    emit(ItemLoading(items: state.items));
     try {
-      await _saveItemUseCase.execute(event.item);
-      emit(state.asSaveSuccess(event.item));
+      final item = await _saveItemUseCase.execute(event.item);
+      emit(ItemLoaded(items: [...state.items, item]));
     } on Exception catch (e) {
-      emit(state.asLoadFailure(e));
+      emit(ItemError(e));
     }
   }
 
   Future<void> _onDeleteItem(DeleteItem event, Emitter<ItemState> emit) async {
-    logger.i('On DeleteItem started..');
-    emit(state.asLoading());
+    emit(ItemLoading(items: state.items));
+    final id = event.item.id;
+    // following block required for null safety
+    if (id == null) {
+      emit(ItemError(Exception('The item contains invalid data.')));
+      return;
+    }
     try {
-      await _saveItemUseCase.execute(event.item);
-      emit(state.copyWith(items: [...state.items, event.item]));
+      await _deleteItemUseCase.execute(id);
+      emit(ItemLoaded(items: state.items.where((i) => i.id != id).toList()));
     } on Exception catch (e) {
-      emit(state.asLoadFailure(e));
+      emit(ItemLoadFailure(e));
     }
   }
 
-  Future<void> _onUpdateItem(DeleteItem event, Emitter<ItemState> emit) async {
-    logger.i('On UpdateItem started..');
-    emit(state.asLoading());
+  Future<void> _onUpdateItem(UpdateItem event, Emitter<ItemState> emit) async {
+    emit(ItemLoading(items: state.items));
     try {
-      await _updateItemUseCase.execute(event.item);
-      final items = state.items.where((i) => event.item.id != i.id).toList();
-      emit(state.copyWith(items: [...items, event.item]));
+      final item = await _updateItemUseCase.execute(event.item);
+      emit(ItemLoaded(
+          items: state.items.map((i) => i.id == item.id ? item : i).toList()));
     } on Exception catch (e) {
-      emit(state.asLoadFailure(e));
+      emit(ItemLoadFailure(e));
     }
   }
-
-// Stream<ItemState> mapEventToState(ItemEvent event) async* {
-//   switch (event.type) {
-//     case ItemActions.add:
-//       yield* _addItem(event.item);
-//       break;
-//       // case ItemActions.remove:
-//       //   yield* _removeItem(event.item);
-//       //   break;
-//       // case ItemActions.like:
-//       //   yield* _likeItem(event.item);
-//       //   break;
-//       // case ItemActions.save:
-//       //   yield* _saveItem(event.item);
-//       break;
-//     default:
-//       break;
-//   }
-// }
-
-// Stream<ItemState> _addItem(Item item) async* {
-//   yield AddingItemState();
-//   try {
-//     await addItemUseCase.execute(item);
-//     yield AddedItemState();
-//   } catch (e) {
-//     yield AddItemFailureState(e);
-//   }
-// }
 }
-
-//***********************************************
-// FROM YOUTUBE VIDEO SEE IF NEEDED
-//***********************************************
 
 abstract class ItemEvent extends Equatable {
   const ItemEvent();
@@ -131,16 +94,14 @@ abstract class ItemEvent extends Equatable {
   List<Object> get props => [];
 }
 
-class LoadsItems extends ItemEvent {
-  const LoadsItems({this.items = const <Item>[]});
+class LoadItem extends ItemEvent {
+  const LoadItem({this.items = const <Item>[]});
 
   final List<Item> items;
 
   @override
   List<Object> get props => [items];
 }
-
-class ItemLoadStarted extends ItemEvent {}
 
 class SaveItem extends ItemEvent {
   const SaveItem(this.item);
@@ -187,4 +148,4 @@ class FindItem extends ItemEvent {
   List<Object> get props => [query];
 }
 
-enum ItemActions { add, delete, like, save }
+// enum ItemActions { add, delete, like, save }
