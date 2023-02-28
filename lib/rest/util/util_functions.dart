c7f023cbf5410454adac9165b7ab2133aa1f255c
i-zanis <ioanlafazanis@gmail.com>
2023-02-28 17:21:56 +0000

    refactor: add temperature conversion functions

--- lib/rest/util/util_functions.dart
+++ lib/rest/util/util_functions.dart
@@ -1,5 +1,14 @@
-
+/// Returns True if the [lastFetched] is older than the [timeToLive].
 bool isStale(DateTime lastFetched, Duration timeToLive) {
   return lastFetched.add(timeToLive).isBefore(DateTime.now());
 }
 
+/// Returns the [kelvin] temperature as a Celsius temperature.
+double kelvinToCelsius(double kelvin) {
+  return kelvin - 273.15;
+}
+
+/// Returns the [kelvin] temperature as a Fahrenheit temperature.
+double kelvinToFahrenheit(double kelvin) {
+  return kelvin * 9 / 5 - 459.67;
+}
