6201101fecc14afd2d95b501a31372bbeba135bc
i-zanis <ioanlafazanis@gmail.com>
2023-03-02 21:07:17 +0000

    refactor: changes to Item Entity

--- lib/domain/entity/item/item.dart
+++ lib/domain/entity/item/item.dart
@@ -1,11 +1,15 @@
 import 'dart:typed_data';
 
+import 'package:Array_App/domain/entity/base_entity.dart';
+import 'package:Array_App/domain/entity/item/tag.dart';
 import 'package:json_annotation/json_annotation.dart';
 
+import 'look.dart';
+
 @JsonSerializable()
-class Item {
+class Item extends BaseEntity {
   Item({
-    this.id,
+    super.id,
     this.createdAt,
     this.name,
     this.colors,
@@ -22,6 +26,8 @@ class Item {
     this.tags,
   });
 
+  Item.empty() : super.empty();
+
   factory Item.fromJson(Map<String, dynamic> json) => Item(
         id: json['id'] as int?,
         createdAt: json['createdAt'] == null
@@ -33,8 +39,9 @@ class Item {
             .toList(),
         brand: json['brand'] as String?,
         category: json['category'] as String?,
-        looks:
-            (json['looks'] as List<dynamic>?)?.map((e) => e as String).toList(),
+        looks: (json['looks'] as List<dynamic>?)
+            ?.map((e) => Look.fromJson(e))
+            .toList(),
         isFavorite: json['isFavorite'] as bool?,
         price: (json['price'] as num?)?.toDouble(),
         userId: json['userId'] as int?,
@@ -44,19 +51,19 @@ class Item {
             : null,
         notes: json['notes'] as String?,
         size: json['size'] as String?,
-        tags:
-            (json['tags'] as List<dynamic>?)?.map((e) => e as String).toList(),
+        tags: (json['tags'] as List<dynamic>?)
+            ?.map((e) => Tag.fromJson(e))
+            .toList(),
       );
 
-  int? id;
   DateTime? createdAt;
   String? name;
   String? brand;
   String? size;
   List<String>? colors;
-  List<String>? tags;
+  List<Tag>? tags;
   String? category;
-  List<String>? looks;
+  List<Look>? looks;
   bool? isFavorite = false;
   double? price;
   int? userId;
@@ -81,46 +88,4 @@ class Item {
         'imageData': imageData,
         'notes': notes,
       };
-// factory Item.fromJson(Map<String, dynamic> json) => _$ItemFromJson(json);
-// Map<String, dynamic> toJson() => _$ItemToJson(this);
 }
-
-// factory Item.fromJson(Map<String, dynamic> json) {
-// return Item(
-// id: json['id'] as int?,
-// createdAt: json['created_at'] as DateTime,
-// name: json['name'] as String?,
-// brand: json['brand'] as String?,
-// colors:
-// (json['colors'] as List<dynamic>?)?.map((e) => e as String).toList(),
-// tags: (json['tag'] as List<dynamic>?)?.map((e) => e as String).toList(),
-// category: json['category'] as String?,
-// looks:
-// (json['looks'] as List<dynamic>?)?.map((e) => e as String).toList(),
-// isFavorite: json['isFavorite'] as bool?,
-// price: json['price'] as double?,
-// userId: json['user'] as int?,
-// imageLocalPath: json['image'] as String?,
-// imageData: json['image'] as Uint8List,
-// notes: json['notes'] as String?,
-// size: json['size'] as String?,
-// );
-// }
-//
-// Map<String, dynamic> toJson(Item instance) => <String, dynamic>{
-// 'id': instance.id,
-// 'created_at': instance.createdAt,
-// 'name': instance.name,
-// 'brand': instance.brand,
-// 'size': instance.size,
-// 'colors': instance.colors?.toList(),
-// 'price': instance.price,
-// 'category': instance.category,
-// 'looks': instance.looks?.toList(),
-// 'isFavorite': instance.isFavorite,
-// 'notes': instance.notes,
-// 'user': instance.userId,
-// 'imageLocalPath': instance.imageLocalPath,
-// 'imageData': instance.imageData,
-// 'tag': instance.tags?.toList(),
-// };
