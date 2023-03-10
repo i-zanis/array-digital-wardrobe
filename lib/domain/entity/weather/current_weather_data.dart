e0a294b4e62ce0add855ebf536e50049db93f14d
i-zanis <ioanlafazanis@gmail.com>
2023-03-10 18:12:11 +0000

    refactor(Weather): replace with linter suggestions

--- lib/domain/entity/weather/current_weather_data.dart
+++ lib/domain/entity/weather/current_weather_data.dart
@@ -1,12 +1,58 @@
 import 'package:Array_App/domain/entity/weather/clouds.dart';
+import 'package:Array_App/domain/entity/weather/coord.dart';
+import 'package:Array_App/domain/entity/weather/forecast_weather_data.dart';
+import 'package:Array_App/domain/entity/weather/main_weather.dart';
 import 'package:Array_App/domain/entity/weather/sys.dart';
 import 'package:Array_App/domain/entity/weather/wind.dart';
 
-import 'coord.dart';
-import 'forecast_weather_data.dart';
-import 'main_weather.dart';
-
 class CurrentWeatherData {
+  CurrentWeatherData({
+    this.coord,
+    this.weather,
+    this.base,
+    this.main,
+    this.visibility,
+    this.wind,
+    this.clouds,
+    this.dt,
+    this.sys,
+    this.timezone,
+    this.id,
+    this.name,
+    this.cod,
+  });
+
+  CurrentWeatherData.fromJson(Map<String, dynamic> json) {
+    coord = json['coord'] != null
+        ? Coord.fromJson(json['coord'] as Map<String, dynamic>)
+        : null;
+    if (json['weather'] != null) {
+      weather = <Weather>[];
+      json['weather'].forEach((v) {
+        weather?.add(Weather.fromJson(v as Map<String, dynamic>));
+      });
+    }
+    base = '';
+    main = json['main'] != null
+        ? Main.fromJson(json['main'] as Map<String, dynamic>)
+        : null;
+    visibility = 0;
+    wind = json['wind'] != null
+        ? Wind.fromJson(json['wind'] as Map<String, dynamic>)
+        : null;
+    clouds = json['clouds'] != null
+        ? Clouds.fromJson(json['clouds'] as Map<String, dynamic>)
+        : null;
+    dt = json['dt'] as int;
+    sys = json['sys'] != null
+        ? Sys.fromJson(json['sys'] as Map<String, dynamic>)
+        : null;
+    timezone = 0;
+    id = 0;
+    name = json['name'] as String;
+    cod = json['cod'] as int;
+  }
+
   late Coord? coord;
   late List<Weather>? weather;
   late String? base;
@@ -20,52 +66,6 @@ class CurrentWeatherData {
   late int? id;
   late String? name;
   late int? cod;
-
-  CurrentWeatherData(
-      {this.coord,
-      this.weather,
-      this.base,
-      this.main,
-      this.visibility,
-      this.wind,
-      this.clouds,
-      this.dt,
-      this.sys,
-      this.timezone,
-      this.id,
-      this.name,
-      this.cod});
-
-  CurrentWeatherData.fromJson(Map<String, dynamic> json) {
-    coord = (json['coord'] != null
-        ? Coord.fromJson(json['coord'] as Map<String, dynamic>)
-        : null)!;
-    if (json['weather'] != null) {
-      weather = <Weather>[];
-      json['weather'].forEach((v) {
-        weather?.add(Weather.fromJson(v as Map<String, dynamic>));
-      });
-    }
-    base = '';
-    main = (json['main'] != null
-        ? Main.fromJson(json['main'] as Map<String, dynamic>)
-        : null)!;
-    visibility = 0;
-    wind = (json['wind'] != null
-        ? Wind.fromJson(json['wind'] as Map<String, dynamic>)
-        : null)!;
-    clouds = (json['clouds'] != null
-        ? Clouds.fromJson(json['clouds'] as Map<String, dynamic>)
-        : null)!;
-    dt = json['dt'] as int;
-    sys = (json['sys'] != null
-        ? Sys.fromJson(json['sys'] as Map<String, dynamic>)
-        : null)!;
-    timezone = 0;
-    id = 0;
-    name = json['name'] as String;
-    cod = json['cod'] as int;
-  }
 
   Map<String, dynamic> toJson() {
     final data = <String, dynamic>{};
