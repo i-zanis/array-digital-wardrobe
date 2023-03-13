3149cb7ae228314fb2681b70c5947da12421fe42
i-zanis <ioanlafazanis@gmail.com>
2023-03-13 20:09:37 +0000

    refactor(logger): remove borders from logging

--- lib/rest/util/log.dart
+++ lib/rest/util/log.dart
@@ -4,11 +4,22 @@ import 'package:logger/logger.dart';
 // In production mode, the logger will be disabled
 
 class Log {
+  static SimplePrinter simplePrinter = SimplePrinter(colors: true);
+
   static final Logger _instance = Logger(
     filter: null,
-    printer: PrettyPrinter(),
+    printer: simplePrinter,
     output: null,
+    // level: Level.info,
   );
 
+  final prettyPrinter = PrettyPrinter(
+    methodCount: 0,
+    errorMethodCount: 0,
+    lineLength: 120,
+    colors: true,
+    printEmojis: true,
+    printTime: false,
+  );
   static Logger get instance => _instance;
 }
