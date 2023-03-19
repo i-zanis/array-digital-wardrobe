6d860775d5baa4fb94585bb3b290910331dfcc20
i-zanis <ioanlafazanis@gmail.com>
2023-03-19 11:49:24 +0000

    feat(widget): add custom linear progress indicator decorator

---
+++ lib/ui/widget/indicator/linear_progress_indicator.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CustomLinearProgressIndicator extends StatelessWidget {
+  const CustomLinearProgressIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final backgroundColor = Theme.of(context).colorScheme.primary;
+    final progressColor = Theme.of(context).colorScheme.onPrimary;
+    return LinearProgressIndicator(
+      backgroundColor: backgroundColor,
+      valueColor: AlwaysStoppedAnimation<Color>(progressColor),
+      semanticsLabel: 'Linear progress indicator',
+    );
+  }
+}
