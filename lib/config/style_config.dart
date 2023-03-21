8568709798d7a972f608dcb6a46e45cd32eaff2e
i-zanis <ioanlafazanis@gmail.com>
2023-03-21 18:55:21 +0000

    feat(config): add StyleConfig

---
+++ lib/config/style_config.dart
@@ -0,0 +1,85 @@
+class StyleConfig {
+  // ******************************* Padding ***********************************
+  static const double paddingS = 8;
+  static const double paddingM = 16;
+  static const double paddingL = 24;
+  static const double defaultPadding = paddingM;
+
+  // ******************************* Margin ************************************
+  static const double marginS = 8;
+  static const double marginM = 16;
+  static const double marginL = 24;
+  static const double defaultMargin = marginM;
+
+  // ******************************* Radius ************************************
+  static const double radiusS = 4;
+  static const double radiusM = 8;
+  static const double radiusL = 16;
+  static const double defaultRadius = radiusM;
+
+  // ******************************* Icon Size *********************************
+  static const double iconSizeS = 16;
+  static const double iconSizeM = 24;
+  static const double iconSizeL = 32;
+  static const double defaultIconSize = iconSizeM;
+
+  // ******************************* Font Size *********************************
+  static const double fontSizeS = 12;
+  static const double fontSizeM = 16;
+  static const double fontSizeL = 20;
+  static const double defaultFontSize = fontSizeM;
+
+  // ******************************* Elevation *********************************
+  static const double elevationS = 2;
+  static const double elevationM = 4;
+  static const double elevationL = 8;
+  static const double defaultElevation = elevationS;
+
+  // ******************************* Border ************************************
+  static const double borderS = 1;
+  static const double borderM = 2;
+  static const double borderL = 4;
+  static const double defaultBorder = borderM;
+
+  // ******************************* Border Radius *****************************
+  static const double borderRadiusS = 4;
+  static const double borderRadiusM = 8;
+  static const double borderRadiusL = 16;
+  static const double defaultBorderRadius = borderRadiusM;
+
+  // ******************************* Opacity ***********************************
+  static const double opacityS = 0.2;
+  static const double opacityM = 0.4;
+  static const double opacityL = 0.6;
+  static const double defaultOpacity = opacityM;
+
+  // ******************************* Height ************************************
+  static const double heightS = 32;
+  static const double heightM = 48;
+  static const double heightL = 64;
+  static const double defaultHeight = heightM;
+
+  // ******************************* Width *************************************
+  static const double widthS = 32;
+  static const double widthM = 48;
+  static const double widthL = 64;
+  static const double defaultWidth = widthM;
+
+  // ******************************* Spacing ***********************************
+  static const double spacingS = 8;
+  static const double spacingM = 16;
+  static const double spacingL = 24;
+  static const double defaultSpacing = spacingM;
+
+  // ******************************* Size **************************************
+  static const double sizeS = 32;
+  static const double sizeM = 48;
+  static const double sizeL = 64;
+  static const double defaultSize = sizeM;
+
+  // *************************** Aspect Ratio **********************************
+  static const double aspectRatioS = 0.5;
+  static const double aspectRatioM = 1;
+  static const double aspectRatioL = 2;
+  static const double defaultAspectRatio = aspectRatioM;
+}
