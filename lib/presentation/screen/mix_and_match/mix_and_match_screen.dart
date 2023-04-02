db196a117000658d2935b887b81488a50eea5229
i-zanis <ioanlafazanis@gmail.com>
2023-04-02 14:11:03 +0100

    feat(screen): add category selection, interaction in MixAndMatchScreen

--- lib/presentation/screen/mix_and_match/mix_and_match_screen.dart
+++ lib/presentation/screen/mix_and_match/mix_and_match_screen.dart
@@ -1,18 +1,31 @@
+import 'package:Array_App/config/style_config.dart';
+import 'package:Array_App/core/route/app_navigator.dart';
 import 'package:Array_App/l10n/l10n.dart';
+import 'package:Array_App/rest/util/util_functions.dart';
 import 'package:flutter/material.dart';
 
-import '../../../config/style_config.dart';
+import '../../../core/route/app_route.dart';
 import '../../widget/custom_app_bar.dart';
+import 'mix_and_match_type.dart';
 
-class MixAndMatchScreen extends StatefulWidget {
-  const MixAndMatchScreen({super.key});
+class MixAndMatchPickScreen extends StatefulWidget {
+  const MixAndMatchPickScreen({super.key});
 
   @override
-  State createState() => _MixAndMatchScreenState();
+  State createState() => _MixAndMatchPickScreenState();
 }
 
-class _MixAndMatchScreenState extends State<MixAndMatchScreen> {
+class _MixAndMatchPickScreenState extends State<MixAndMatchPickScreen> {
   List<int> selectedBoxes = [];
+
+  final boxText = <Category>[
+    Category.top,
+    Category.bottom,
+    Category.shoes,
+    Category.accessories,
+    Category.innerwear,
+    Category.other,
+  ];
 
   @override
   void initState() {
@@ -37,6 +50,10 @@ class _MixAndMatchScreenState extends State<MixAndMatchScreen> {
       appBar: CustomAppBar(
         title: l10n.mixAndMatchScreenTitle,
         subtitle: l10n.mixAndMatchScreenSubtitle,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(StyleConfig.defaultMargin),
@@ -47,6 +64,7 @@ class _MixAndMatchScreenState extends State<MixAndMatchScreen> {
               crossAxisCount: 2,
               children: List.generate(6, (index) {
                 final boxColor = backgroundColors[index % 3];
+                final text = boxText[index];
                 return GestureDetector(
                   onTap: () {
                     setState(() {
@@ -71,7 +89,7 @@ class _MixAndMatchScreenState extends State<MixAndMatchScreen> {
                     ),
                     child: Center(
                       child: Text(
-                        'Container ${index + 1}',
+                        text.name.toUpperCase(),
                         style: Theme.of(context).textTheme.bodyLarge?.apply(
                               color: textColors[index % 3],
                             ),
@@ -82,14 +100,28 @@ class _MixAndMatchScreenState extends State<MixAndMatchScreen> {
               }),
             ),
             const Spacer(),
-            FilledButton(onPressed: () => {}, child: Text('Continue')),
+            FilledButton(
+              onPressed: () =>
+                  _handMatchStyle(l10n.mixAndMatchNoCategorySelectedError),
+              child: Text(l10n.mixAndMatchScreenButtonContinue),
+            ),
           ],
         ),
       ),
     );
   }
 
-  void _matchStyle() {}
+  void _handMatchStyle(String snackBarContent) {
+    if (selectedBoxes.isEmpty) {
+      showSnackBar(context, snackBarContent);
+    } else {
+      final itemsToMoveToNextScreen = <Category>[];
+      for (final index in selectedBoxes) {
+        itemsToMoveToNextScreen.add(boxText[index]);
+      }
+      // logger.d(itemsToMoveToNextScreen);
+      AppNavigator.push(AppRoute.mixAndMatchResult, itemsToMoveToNextScreen);
+      // Navigator.push(context, MixAndMatchPickScreen(itemsToMoveToNextScreen));
+    }
+  }
 }
-
-enum MixAndMatchType { top, bottom, shoes, accessories, innerwear, other }
