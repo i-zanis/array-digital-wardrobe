8b2da67d2ba5e30685f9c3873d3d32b6a4aece99
i-zanis <ioanlafazanis@gmail.com>
2023-04-05 21:01:31 +0100

    refactor(widget): change custom sized boxes to const

---
+++ lib/presentation/widget/constant/sized_box.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+const sizedBox4 = SizedBox(height: 4);
+const sizedBox8 = SizedBox(height: 8);
+const sizedBox16 = SizedBox(height: 16);
+const sizedBox24 = SizedBox(height: 24);
+const sizedBox32 = SizedBox(height: 32);
---
+++ lib/presentation/widget/widget.dart
@@ -0,0 +1,6 @@
+export 'button/button.dart';
+export 'constant/sized_box.dart';
+export 'custom_app_bar.dart';
+export 'indicator/custom_linear_progress_indicator.dart';
+export 'weather/weather_functions.dart';
+export 'weather/weather_icon.dart';
