8fe9bfbd6a98641cbdcd6e75a306aa0916b95e2c
i-zanis <ioanlafazanis@gmail.com>
2023-04-07 16:59:22 +0100

    refactor(widget): refactor const sized box to classes

---
+++ lib/presentation/widget/constant/box.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+/// A widget that creates a fixed-height [SizedBox].
+class Box extends SizedBox {
+  const Box({super.key});
+
+  static const SizedBox h7 = SizedBox(height: 7);
+  static const SizedBox h8 = SizedBox(height: 8);
+  static const SizedBox h16 = SizedBox(height: 16);
+  static const SizedBox h32 = SizedBox(height: 32);
+  static const SizedBox h40 = SizedBox(height: 40);
+  static const SizedBox h48 = SizedBox(height: 48);
+
+  static const SizedBox w7 = SizedBox(width: 7);
+  static const SizedBox w8 = SizedBox(width: 8);
+  static const SizedBox w16 = SizedBox(width: 16);
+  static const SizedBox w32 = SizedBox(width: 32);
+  static const SizedBox w40 = SizedBox(width: 40);
+  static const SizedBox w48 = SizedBox(width: 48);
+}
