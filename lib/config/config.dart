856c28677319351f44dcfa6e054368fa6b3cc264
i-zanis <ioanlafazanis@gmail.com>
2023-04-13 22:22:04 +0100

    refactor(config): update removebg, weatherapi urls

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,6 +1,11 @@
 class Config {
+  // ******************************* URLS **************************************
   static const String baseUrl = 'http://192.168.1.104:8080/v1';
   static const String itemsUrl = '$baseUrl/items';
   static const String weatherUrl =
       'https://api.openweathermap.org/data/2.5/weather';
+  static const String removeBackgroundUrl =
+      'https://clipdrop-api.co/remove-background/v1';
+
+  static const String baseLocation = 'London';
 }
