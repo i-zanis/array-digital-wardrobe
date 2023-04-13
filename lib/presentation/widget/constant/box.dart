c5ae69ca5309637beb45819e8794fc0fdb0a807e
i-zanis <ioanlafazanis@gmail.com>
2023-04-13 19:22:23 +0100

    refactor(widget): add more SizedBox constants

--- lib/presentation/widget/constant/box.dart
+++ lib/presentation/widget/constant/box.dart
@@ -7,6 +7,8 @@ class Box extends SizedBox {
   static const SizedBox h7 = SizedBox(height: 7);
   static const SizedBox h8 = SizedBox(height: 8);
   static const SizedBox h16 = SizedBox(height: 16);
+  static const SizedBox h24 = SizedBox(height: 24);
+  static const SizedBox h28 = SizedBox(height: 28);
   static const SizedBox h32 = SizedBox(height: 32);
   static const SizedBox h40 = SizedBox(height: 40);
   static const SizedBox h48 = SizedBox(height: 48);
@@ -14,6 +16,8 @@ class Box extends SizedBox {
   static const SizedBox w7 = SizedBox(width: 7);
   static const SizedBox w8 = SizedBox(width: 8);
   static const SizedBox w16 = SizedBox(width: 16);
+  static const SizedBox w24 = SizedBox(width: 24);
+  static const SizedBox w28 = SizedBox(width: 28);
   static const SizedBox w32 = SizedBox(width: 32);
   static const SizedBox w40 = SizedBox(width: 40);
   static const SizedBox w48 = SizedBox(width: 48);
