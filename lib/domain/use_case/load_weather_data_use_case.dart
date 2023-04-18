cd0ae8477226c5d9f6569eec4185d15884158fce
i-zanis <ioanlafazanis@gmail.com>
2023-04-18 22:16:43 +0100

    refactor(weather): remove negative conditional names

--- lib/domain/use_case/load_weather_data_use_case.dart
+++ lib/domain/use_case/load_weather_data_use_case.dart
@@ -1,6 +1,6 @@
 import 'package:Array_App/data/repository/weather_repository_impl.dart';
 import 'package:Array_App/domain/entity/weather/current_weather_data.dart';
-import 'package:Array_App/domain/exception/weather_exception.dart';
+import 'package:Array_App/domain/exception/exception.dart';
 import 'package:Array_App/domain/repository/weather_repository.dart';
 import 'package:Array_App/domain/use_case/use_case.dart';
 import 'package:Array_App/main_development.dart';
@@ -15,7 +15,7 @@ class LoadWeatherUseCase implements UseCase<CurrentWeatherData, String> {
   Future<CurrentWeatherData> execute(String city) async {
     logger.i('$LoadWeatherUseCase execute city: $city');
     if (await validate(city)) {
-      return _weatherRepository.getWeather(city);
+      return _weatherRepository.findWeatherDataByCity(city);
     }
     return Future.error(WeatherUseCaseException('Invalid city name'));
   }
@@ -26,10 +26,10 @@ class LoadWeatherUseCase implements UseCase<CurrentWeatherData, String> {
     if (sanitizedCity.isEmpty) {
       throw WeatherUseCaseException("City name can't be empty");
     }
-    if (!isValidCityName(sanitizedCity)) {
+    if (_isInvalidCityName(sanitizedCity)) {
       throw WeatherUseCaseException('City name contains invalid characters');
     }
-    if (!_isCityNameLengthValid(sanitizedCity)) {
+    if (_isInvalidCityNameLength(sanitizedCity)) {
       throw WeatherUseCaseException(
         'City name should be between 2 and 60 characters',
       );
@@ -37,12 +37,12 @@ class LoadWeatherUseCase implements UseCase<CurrentWeatherData, String> {
     return true;
   }
 
-  bool isValidCityName(String value) {
+  bool _isInvalidCityName(String value) {
     // r == raw string
-    return RegExp(r'^[a-zA-Z]+$').hasMatch(value);
+    return !RegExp(r'^[a-zA-Z]+$').hasMatch(value);
   }
 
-  bool _isCityNameLengthValid(String cityName) {
-    return cityName.length >= 2 && cityName.length <= 60;
+  bool _isInvalidCityNameLength(String cityName) {
+    return cityName.isEmpty || cityName.length > 60;
   }
 }
