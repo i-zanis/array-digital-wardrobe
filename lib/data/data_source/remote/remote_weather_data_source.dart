b984de006e7ff45459ebed14f7de4924aeb3cd8e
i-zanis <ioanlafazanis@gmail.com>
2023-04-22 14:34:07 +0100

    refactor(weather): remove unnecessary json conversions

--- lib/data/data_source/remote/remote_weather_data_source.dart
+++ lib/data/data_source/remote/remote_weather_data_source.dart
@@ -1,17 +1,15 @@
-import 'dart:convert';
-
 import 'package:Array_App/config/config.dart';
+import 'package:Array_App/data/network/network_client_factory.dart';
 import 'package:Array_App/domain/entity/weather/current_weather_data.dart';
 import 'package:Array_App/main_development.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import '../../network/network_client.dart';
-import '../../network/network_client_factory.dart';
 
 class RemoteWeatherDataSource {
   RemoteWeatherDataSource({NetworkClient? client, String? baseUrl})
       : _client = client ?? NetworkClientFactory.create(),
-        _baseUrl = baseUrl ?? Config.weatherUrl;
+        _baseUrl = baseUrl ?? Config.weatherApiUrl;
 
   final NetworkClient _client;
   final String _baseUrl;
@@ -21,10 +19,9 @@ class RemoteWeatherDataSource {
     final response = await _client.get(
       "$_baseUrl?q=$city,uk&appid=${dotenv.env['OPEN_WEATHER_MAP_API_KEY']}",
     );
-    logger.i(
+    logger.d(
       '$RemoteWeatherDataSource: getWeatherData($city) response: $response',
     );
-    final json = await jsonDecode(jsonEncode(response.data));
-    return CurrentWeatherData.fromJson(json as Map<String, dynamic>);
+    return CurrentWeatherData.fromJson(response.data as Map<String, dynamic>);
   }
 }
