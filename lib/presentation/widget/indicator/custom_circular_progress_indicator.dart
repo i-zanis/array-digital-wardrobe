ece5f0f3b0d6a60287b1b5aa24385f9bf657d448
i-zanis <ioanlafazanis@gmail.com>
2023-04-22 14:16:01 +0100

    feat(widget): add CircularProgressIndicator

---
+++ lib/presentation/widget/indicator/custom_circular_progress_indicator.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CustomCircularProgressIndicator extends StatelessWidget {
+  const CustomCircularProgressIndicator({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: 150,
+      width: 150,
+      child: CircularProgressIndicator(),
+    );
+  }
+}
