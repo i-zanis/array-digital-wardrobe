da5ae6d373bb67c9cf7147b686f957be406c2261
i-zanis <ioanlafazanis@gmail.com>
2023-04-26 21:18:01 +0100

    refactor(util): update functions

--- lib/rest/util/util_functions.dart
+++ lib/rest/util/util_functions.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'dart:ui' as ui;
 
+import 'package:Array_App/domain/entity/entity.dart';
+import 'package:Array_App/l10n/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -23,25 +25,12 @@ Future<Uint8List> imageToUint8list(String key) async {
   return (await rootBundle.load(key)).buffer.asUint8List();
 }
 
-// Future<ui.Image> uint8ListToImage(Uint8List? byteData) async {
-//   if (byteData == null) {
-//     return Future.value();
-//   }
-//   final codec = await ui.instantiateImageCodec(byteData);
-//   final frameInfo = await codec.getNextFrame();
-//   return frameInfo.image;
-// }
-
-Future<ui.Image> uint8ListToImage(Uint8List? img) async {
+Future<ui.Image> uInt8ListToImage(Uint8List? img) async {
   if (img == null) {
     return Future.value();
   }
-  final Completer<ui.Image> completer = Completer();
-
-  ui.decodeImageFromList(img, (ui.Image img) {
-    return completer.complete(img);
-  });
-
+  final completer = Completer<ui.Image>();
+  ui.decodeImageFromList(img, completer.complete);
   return completer.future;
 }
 
@@ -50,7 +39,7 @@ Future<Uint8List> imageAssetToUint8List({
   required int width,
   required int height,
 }) async {
-  final ByteData data = await rootBundle.load(source);
+  final data = await rootBundle.load(source);
 
   final codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
       targetWidth: width, targetHeight: height);
@@ -68,10 +57,12 @@ void showSnackBar(BuildContext context, String content) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
       backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
-      content: Text(content,
-          style: Theme.of(context).textTheme.bodyMedium?.apply(
-                color: Theme.of(context).colorScheme.onSecondaryContainer,
-              )),
+      content: Text(
+        content,
+        style: Theme.of(context).textTheme.bodyMedium?.apply(
+              color: Theme.of(context).colorScheme.onSecondaryContainer,
+            ),
+      ),
       duration: const Duration(
         seconds: 2,
       ),
@@ -81,6 +72,12 @@ void showSnackBar(BuildContext context, String content) {
       //   ),
     ),
   );
+}
+
+Color getItemBackgroundColor(BuildContext context) {
+  return Theme.of(context).colorScheme.surfaceVariant.withOpacity(
+        0.4,
+      );
 }
 
 T getValueOrDefault<T>(dynamic value, T defaultValue) {
@@ -107,4 +104,18 @@ String getStringOrDefault(dynamic value, [String defaultValue = '']) {
     return value.toString();
   }
   return value.toString();
+}
+
+String getCategoryName(BuildContext context, Enum? category) {
+  final l10n = context.l10n;
+  final categoryMap = {
+    Category.TOP: l10n.categoryTop,
+    Category.BOTTOM: l10n.categoryBottom,
+    Category.SHOES: l10n.categoryShoes,
+    Category.ACCESSORIES: l10n.categoryAccessories,
+    Category.INNERWEAR: l10n.categoryInnerwear,
+    Category.OTHER: l10n.categoryOther,
+  };
+  final name = categoryMap[category] ?? l10n.categoryOther;
+  return name.toUpperCase();
 }
