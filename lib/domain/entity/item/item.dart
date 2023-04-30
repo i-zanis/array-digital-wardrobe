470c0d9dd010b7b7a83f22dd81b46e4abff2c040
i-zanis <ioanlafazanis@gmail.com>
2023-04-30 08:46:44 +0100

    refactor(entity): add copyWith(), improved toString() to Item

--- lib/domain/entity/item/item.dart
+++ lib/domain/entity/item/item.dart
@@ -1,8 +1,7 @@
 import 'dart:typed_data';
 
 import 'package:Array_App/domain/entity/base_entity.dart';
-import 'package:Array_App/domain/entity/item/look.dart';
-import 'package:Array_App/domain/entity/item/tag.dart';
+import 'package:Array_App/domain/entity/entity.dart';
 import 'package:Array_App/domain/entity/item/u_int_8_list_converter.dart';
 import 'package:json_annotation/json_annotation.dart';
 
@@ -17,7 +16,6 @@ class Item extends BaseEntity {
     this.colors,
     this.brand,
     this.category,
-    this.looks,
     this.isFavorite,
     this.price,
     this.userId,
@@ -28,11 +26,23 @@ class Item extends BaseEntity {
     this.tags,
   });
 
+  Item.empty() : super.empty();
+
   factory Item.fromJson(Map<String, dynamic> json) => _$ItemFromJson(json);
 
   Map<String, dynamic> toJson() => _$ItemToJson(this);
 
-  Item.empty() : super.empty();
+  @override
+  String toString() {
+    return 'Item {'
+        '  id: $id,'
+        '  name: $name,'
+        '  category: $category,'
+        '  isFavorite: $isFavorite,'
+        '  imageData: ${imageData != null ? 'Present' : 'Not present'},'
+        '  tags: $tags'
+        '}';
+  }
 
   DateTime? createdAt;
   String? name;
@@ -40,8 +50,7 @@ class Item extends BaseEntity {
   String? size;
   List<String>? colors;
   List<Tag>? tags;
-  String? category;
-  List<Look>? looks;
+  Category? category;
   bool? isFavorite = false;
   double? price;
   int? userId;
@@ -50,52 +59,37 @@ class Item extends BaseEntity {
   Uint8List? imageData;
   String? notes;
 
-// Map<String, dynamic> toJson() => <String, dynamic>{
-//       'id': id,
-//       'createdAt': createdAt?.toIso8601String(),
-//       'name': name,
-//       'brand': brand,
-//       'size': size,
-//       'colors': colors,
-//       'tags': tags?.map((e) => e.toJson()).toList(),
-//       'category': category,
-//       'looks': looks,
-//       'isFavorite': isFavorite,
-//       'price': price,
-//       'userId': userId,
-//       'imageLocalPath': imageLocalPath,
-//       // 'imageData': imageData != null ? List<int>.from(imageData!) : null,
-//       'notes': notes,
-//     };
-// factory Item.fromJson(Map<String, dynamic> json) => Item(
-//       id: json['id'] as int?,
-//       createdAt: json['createdAt'] == null
-//           ? null
-//           : DateTime.parse(json['createdAt'] as String),
-//       name: json['name'] as String?,
-//       colors: (json['colors'] as List<dynamic>?)
-//           ?.map((e) => e as String)
-//           .toList(),
-//       brand: json['brand'] as String?,
-//       category: json['category'] as String?,
-//       looks: (json['looks'] as List<dynamic>?)
-//           ?.map((e) => Look.fromJson(e as Map<String, dynamic>))
-//           .toList(),
-//       isFavorite: json['isFavorite'] as bool?,
-//       price: (json['price'] as num?)?.toDouble(),
-//       userId: json['userId'] as int?,
-//       imageLocalPath: json['imageLocalPath'] as String?,
-//       // imageData: json['imageData'] != null
-//       //     ? Uint8List.fromList(
-//       //         (json['imageData'] as List<dynamic>)
-//       //             .map((e) => e as int)
-//       //             .toList(),
-//       //       )
-//       //     : null,
-//       notes: json['notes'] as String?,
-//       size: json['size'] as String?,
-//       tags: (json['tags'] as List<dynamic>?)
-//           ?.map((e) => Tag.fromJson(e as Map<String, dynamic>))
-//           .toList(),
-//     );
+  Item copyWith({
+    String? id,
+    DateTime? createdAt,
+    String? name,
+    String? brand,
+    String? size,
+    List<String>? colors,
+    List<Tag>? tags,
+    Category? category,
+    bool? isFavorite,
+    double? price,
+    int? userId,
+    String? imageLocalPath,
+    Uint8List? imageData,
+    String? notes,
+  }) {
+    return Item(
+      id: super.id ?? this.id,
+      createdAt: createdAt ?? this.createdAt,
+      name: name ?? this.name,
+      brand: brand ?? this.brand,
+      size: size ?? this.size,
+      colors: colors ?? this.colors,
+      tags: tags ?? this.tags,
+      category: category ?? this.category,
+      isFavorite: isFavorite ?? this.isFavorite,
+      price: price ?? this.price,
+      userId: userId ?? this.userId,
+      imageLocalPath: imageLocalPath ?? this.imageLocalPath,
+      imageData: imageData ?? this.imageData,
+      notes: notes ?? this.notes,
+    );
+  }
 }
