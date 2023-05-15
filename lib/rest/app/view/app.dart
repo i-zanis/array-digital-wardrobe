9dd589ebb819aab6b4a5097b110cd337f781e7f0
i-zanis <ioanlafazanis@gmail.com>
2023-05-15 08:49:56 +0100

    chore(app): remove unnecessary color declaration

--- lib/rest/app/view/app.dart
+++ lib/rest/app/view/app.dart
@@ -1,8 +1,7 @@
-import 'package:Array_App/config/theme.dart';
+import 'package:Array_App/config/array_theme.dart';
+import 'package:Array_App/core/route/app_navigator.dart';
+import 'package:Array_App/l10n/l10n.dart';
 import 'package:flutter/material.dart';
-
-import '../../../core/route/app_navigator.dart';
-import '../../../l10n/l10n.dart';
 
 class ArrayApp extends StatelessWidget {
   const ArrayApp({super.key});
@@ -13,7 +12,6 @@ class ArrayApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ArrayTheme.light,
       darkTheme: ArrayTheme.dark,
-      color: Colors.white,
       title: 'Array Digital Wardrobe Application',
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
