7aff17dcfee2f9badde0e3fe35d5b26c9aea413c
i-zanis <ioanlafazanis@gmail.com>
2023-06-03 10:46:23 +0100

    refactor(main): add Camera, env file initial loading

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,24 +1,23 @@
+import 'package:Array_App/bootstrap.dart';
+import 'package:Array_App/rest/app/view/app.dart';
+import 'package:Array_App/rest/util/logger/log.dart';
+import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:wardrobe_frontend/bootstrap.dart';
-import 'package:wardrobe_frontend/rest/app/view/app.dart';
-import 'package:wardrobe_frontend/rest/util/log.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final logger = Log.instance;
+SharedPreferences? sharedPrefs;
+
+List<CameraDescription> cameras = <CameraDescription>[];
 
 Future<void> main() async {
+  // TODO MAYBE needs removal because it's inside runapp in Bootstrap
   // required if main is async
   WidgetsFlutterBinding.ensureInitialized();
   // boostrap == BlocOberser file/class
-  await bootstrap(() => const ArrayApp());
-
+  sharedPrefs = await SharedPreferences.getInstance();
+  await dotenv.load();
+  cameras = await availableCameras();
+  await bootstrap(ArrayApp.new);
 }
-
-// void main() {
-//   runApp(BlocProvider<WardrobeBloc>(
-//     create: (context) => WardrobeBloc(
-//       itemBloc: ItemBloc(),
-//       userBloc: UserBloc(),
-//     ),
-//     child: MyApp(),
-//   ));
-// }
